276d980a6ad13b8f0f02271bfe5d54bb6e885c75
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-09-10 22:33:59 -0300

    feat: created ProjectCard

--- src/components/ProjetcCard/index.jsx
+++ src/components/ProjetcCard/index.jsx
@@ -1,9 +1,35 @@
 import { ProjectCardContainer } from "./ProjectCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrochip } from "@fortawesome/free-solid-svg-icons";
+
 const ProjectCard = ({ project }) => {
   return (
     <ProjectCardContainer>
-      {project.title}
-      <img src={project.img_print}></img>
+      <div>
+        <div className="info">
+          <img src={project.img_print} alt="Project print"></img>
+          <div className="info-text">
+            <h1>{project.title}</h1>
+            <p>{project.description}</p>
+          </div>
+        </div>
+        <div className="tech-container">
+          <ul>
+            <FontAwesomeIcon icon={faMicrochip} className="icon-color" />
+            {project.techs.map((element) => {
+              return <li>{element}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="buttons-container">
+          <a href={project.repo_url} target="_blank" rel="noreferrer">
+            repo
+          </a>
+          <a href={project.demo_url} target="_blank" rel="noreferrer">
+            demo
+          </a>
+        </div>
+      </div>
     </ProjectCardContainer>
   );
 };
