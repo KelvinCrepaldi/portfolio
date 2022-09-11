598e4498634a16f422611432e7195c61ea6f4175
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-09-11 05:34:03 -0300

    feat: initialized contact form

--- src/pages/Contact/Contact.styles.js
+++ src/pages/Contact/Contact.styles.js
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
-export const ContactContainer = styled.div``;
+export const ContactContainer = styled.div`
+  .form {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+
+  input {
+    width: 300px;
+  }
+
+  button {
+    width: 200px;
+  }
+`;
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,13 +1,48 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
 import { ContactContainer } from "./Contact.styles";
 import ContentContainer from "../../components/ContentContainer ";
 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Por favor escreva seu nome."),
+    email: yup
+      .string()
+      .required("Por favor escreva seu e-mail")
+      .email("Formato de e-mail inválido"),
+    message: yup.string().required("Por favor escreva uma mensagem"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(formSchema) });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+  };
+
   return (
     <ContactContainer>
       <ContentContainer title="Contato" icon={faEnvelope}>
-        Portfolio content
+        <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
+          <input placeholder="Nome" {...register("name")} />
+          {errors.name?.message}
+          <input placeholder="Email" {...register("email")} />
+          {errors.email?.message}
+          <textarea
+            type="text"
+            placeholder="Mensagem"
+            {...register("message")}
+          />
+          {errors.message?.message}
+          <button type="submit">Enviar mensagem!</button>
+        </form>
       </ContentContainer>
     </ContactContainer>
   );
